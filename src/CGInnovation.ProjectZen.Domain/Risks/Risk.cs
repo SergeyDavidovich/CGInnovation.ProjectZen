8627b46603c3f4d59ed83834d56ac7e068115f74
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-11-09 00:52:02 +0100

    added domain classes and project documents

---
+++ src/CGInnovation.ProjectZen.Domain/Risks/Risk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace CGInnovation.ProjectZen.Risks
+{
+    public class Risk : AuditedAggregateRoot<Guid>// Type of the primary key of the Risk
+    {
+
+    }
+}
+
+
+//-Risk Scoring
+//- Likelihood(1 - 10, 10 highest)
+//- Impact(1 - 10, 10 highest)
+//- Risk Data
+//- Created Date
+//- Updated Date
+//- Created By
+//- Updated By
+//- Mitigation(Full, Partial, None)
+//- Mitigation Description(Text)
+//- Occurred(Yes / No)
+//- Occurred Date
---
+++ src/CGInnovation.ProjectZen.Domain/Strategies/Strategy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace CGInnovation.ProjectZen.Strategies
+{
+    
+    public class Strategy : AuditedAggregateRoot<Guid>// Type of the primary key of the Strategy
+    {
+        public Guid StrategyId { get; set; }
+
+        public string Name { get; set; }
+        
+    }
+}
