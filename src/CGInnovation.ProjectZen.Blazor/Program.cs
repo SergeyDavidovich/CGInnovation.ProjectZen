046081808ca764125635402c0f9a0c8d943d6e8d
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-11-20 00:51:19 +0100

    implement test integration Syncfusion Grid component

--- src/CGInnovation.ProjectZen.Blazor/Program.cs
+++ src/CGInnovation.ProjectZen.Blazor/Program.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
 
 namespace CGInnovation.ProjectZen.Blazor
 {
@@ -7,6 +9,9 @@ namespace CGInnovation.ProjectZen.Blazor
     {
         public static async Task Main(string[] args)
         {
+            // Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTM3MDQ5QDMxMzkyZTMzMmUzMGRwSnVQcnFwVENNWWdGU2JralJ2aXlHM1FMcmFMVzAvWlZsdUl2UG9HaHM9");
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
             var application = builder.AddApplication<ProjectZenBlazorModule>(options =>
@@ -14,6 +19,8 @@ namespace CGInnovation.ProjectZen.Blazor
                 options.UseAutofac();
             });
 
+            builder.Services.AddSyncfusionBlazor();
+            
             var host = builder.Build();
 
             await application.InitializeAsync(host.Services);
