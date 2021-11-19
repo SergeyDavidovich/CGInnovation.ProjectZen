8862226677b3192648b17c47ef7b89b88669c05b
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-11-19 23:06:59 +0100

    added RiskInProject entity and RiskMonitorBlazor component

---
+++ src/CGInnovation.ProjectZen.Domain/RisksInProjects/RiskInProject.cs
@@ -0,0 +1,21 @@
+﻿using CGInnovation.ProjectZen.Risks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.Domain.Entities.Auditing;
+
+
+namespace CGInnovation.ProjectZen.RisksInProjects
+{
+    public class RiskInProject : AuditedAggregateRoot<Guid>, IMultiTenant
+    {
+        public Guid RiskId { get; set; }
+        public Guid ProjectId { get; set; }
+        public DateTime DateTime{get;set;}
+        public Guid? TenantId{get;set;}
+        public Mitigation Mitigation{get;set;}
+    }
+}
